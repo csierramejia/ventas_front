869d102d6f61229086251d180067800103602b77
haroldtaborda <haroldtaborda93@gmail.com>
2020-07-09 14:12:40 -0500

    se agrega la hora

--- ventas-front/src/app/modules/productos/chance/apuesta/apuesta.component.ts
+++ ventas-front/src/app/modules/productos/chance/apuesta/apuesta.component.ts
@@ -675,7 +675,7 @@ export class ApuestaComponent extends CommonComponent implements OnInit, OnDestr
    */
   setDays(dayWeek): void {
     const hoy = new Date();
-    const dt = new Date(hoy.getMonth() + 1 + ' ' + hoy.getDate() + ', ' + hoy.getFullYear());
+    const dt = new Date();
     switch (dayWeek) {
       case 'lun':
         this.days[0].date = dt;
