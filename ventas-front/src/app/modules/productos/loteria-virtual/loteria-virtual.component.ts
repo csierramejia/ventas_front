2f731232f8a5522bdbe0b8377e06a450fa71c8ce
Your Name <you@example.com>
2020-07-25 16:39:47 -0500

    Ajustes para la funcionalidad loteria virtual

--- ventas-front/src/app/modules/productos/loteria-virtual/loteria-virtual.component.ts
+++ ventas-front/src/app/modules/productos/loteria-virtual/loteria-virtual.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonComponent } from 'src/app/utilities/common.component';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { SpinnerState } from 'src/app/states/spinner.state';
 
+/**
+ * Componente para las ventas de las loterias virtual
+ */
 @Component({
-  selector: 'app-loteria-virtual',
   templateUrl: './loteria-virtual.component.html',
   styleUrls: ['./loteria-virtual.component.css']
 })
-export class LoteriaVirtualComponent implements OnInit {
+export class LoteriaVirtualComponent extends CommonComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  /**
+   * @param messageService, Se utiliza para la visualizacion
+   * de los mensajes en la pantalla
+   *
+   * @param confirmationService, se utiliza para mostrar el
+   * modal de confirmacion para diferente procesos
+   *
+   * @param spinnerState, se utiliza para simular el spinner
+   */
+  constructor(
+    protected messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private spinnerState: SpinnerState) {
+    super();
   }
 
+  /**
+   * Se debe inicializar las variables cuando se crea el component
+   */
+  ngOnInit(): void {
+    this.init();
+  }
+
+  /**
+   * Se utiliza para limpiar los mensajes visualizados pantalla
+   */
+  ngOnDestroy(): void {
+    this.messageService.clear();
+  }
+
+  /**
+   * Metodo que es invocado al momento de la creacion
+   * del componente, donde se procede a consultar los
+   * datos iniciales requeridos de la funcionalidad
+   */
+  private init(): void {
+  }
 }
