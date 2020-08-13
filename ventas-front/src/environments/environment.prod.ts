57eea380365b6b6884c627e7d65ddec757559798
haroldtaborda <haroldtaborda93@gmail.com>
2020-08-13 10:13:32 -0500

    se actualiza prod

--- ventas-front/src/environments/environment.prod.ts
+++ ventas-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverApiUrl: "http://ecsalb02-781733494.us-east-1.elb.amazonaws.com:8181/",
+  serverApiUrl: "http://gateway.logiiblue.com:8181/",
 };
